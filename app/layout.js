034d88ae977a81e39c7e57028578ec7a8ccdcdf1
jacodore <jaco.dorefice03@gmail.com>
2025-02-17 22:01:02 +0100

    Finale globals.css completo con Tailwind + personalizzazioni

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,15 @@
+import { Poppins } from 'next/font/google';
 import './globals.css';
+
+const poppins = Poppins({
+  weight: ['300', '400', '600'],
+  subsets: ['latin'],
+});
 
 export default function Layout({ children }) {
   return (
-    <html lang="it">
-      <body className="bg-black text-white min-h-screen">{children}</body>
+    <html lang="it" className={poppins.className}>
+      <body>{children}</body>
     </html>
   );
 }
