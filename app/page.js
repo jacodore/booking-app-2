d00b614c75ab33108c8d7ba9ae21cd522c081f75
jacodore <jaco.dorefice03@gmail.com>
2025-02-17 20:43:41 +0100

    Aggiunta use client a page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from 'react';
 import { Home, Calendar, Ticket, MapPin, User, Search, Map, QrCode, MessageSquare } from 'lucide-react';
 import { Input } from '@/components/ui/input';
